b2f22873dc02b5d985f822feea03d77716bcaba1
Mark Mims <mark.mims@ubuntu.com>
2018-02-06 11:18:29 -0700

    minimal memory footprint

--- docker_files/cdh_startup_script.sh
+++ docker_files/cdh_startup_script.sh
@@ -2,20 +2,20 @@
 echo "Start HDFS"
 bash -c 'for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done'
 
-echo "Start Yarn"
-service hadoop-yarn-resourcemanager start
-service hadoop-yarn-nodemanager start
-service hadoop-mapreduce-historyserver start
-service oozie start
+#echo "Start Yarn"
+#service hadoop-yarn-resourcemanager start
+#service hadoop-yarn-nodemanager start
+#service hadoop-mapreduce-historyserver start
+#service oozie start
 
 echo "Start Components"
-service hue start
+#service hue start
 
 nohup hiveserver2 & #TODO make this a real service
 
-bash -c 'for x in `cd /etc/init.d ; ls impala-*` ; do sudo service $x start ; done'
-service hbase-master start
-service hbase-thrift start
+#bash -c 'for x in `cd /etc/init.d ; ls impala-*` ; do sudo service $x start ; done'
+#service hbase-master start
+#service hbase-thrift start
 
 echo "Press Ctrl+P and Ctrl+Q to background this process."
 echo 'Use exec command to open a new bash instance for this instance (Eg. "docker exec -i -t CONTAINER_ID bash"). Container ID can be obtained using "docker ps" command.'
